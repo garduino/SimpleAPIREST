0838c0a4abe091a9c5941694a12ed989706ca83b
garduino <garduino@gmail.com>
2018-10-12 22:55:06 -0300

    Hasta Lección 9.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const Product = require('./models/product')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,19 +13,45 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get('/api/product', (req,res) => {
-	res.status(200).send({products: []})
+	// find{} devuelve todos los productos
+	Product.find({}, (err, product) => {
+		if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`} )
+		if (!product) return res.status(404).send({message: `No existen productos` })
+
+		res.send(200,{product})
+
+	})
+
 })
 
 app.get('/api/product/:productId', (req,res) => {
+	let productId = req.params.productId
+	Product.findById(productId, (err, product) => {
+		if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`} )
+		if (!product) return res.status(404).send({message: `El producto no existe` })
 
+		res.status(200).send({product})
+		// = a res.status(200).send({product; product})
+	})
 
 })
 
 
 app.post('/api/product', (req,res) => {
-	
+	console.log('POST api/product')
 	console.log(req.body)
-	res.status(200).send({message: 'El producto se ha recibido'})
+
+	let product = new Product()
+	product.name = req.body.name
+	product.picture = req.body.picture
+	product.price = req.body.price
+	product.category = req.body.category
+	product.description = req.body.description
+
+	product.save((err, productStored) => {
+		if (err) res.status(500).send({message: `Error al guardar en la base de datos: ${err}`})
+		res.status(200).send({product: productStored})
+	})
 
 })
 
@@ -37,8 +66,16 @@ app.delete('/api/product/:productId', (req,res) => {
 
 })
 
-app.listen(port, () => {
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+	if (err) {
+		return console.log(`Error al conectar a la base de datos: ${err}`)
+	}
+	console.log('Conexión a la base de datos establecida...')
 
-	console.log( `API REST corriendo en http://localhost:${port}`)
+	app.listen(port, () => {
 
+		console.log( `API REST corriendo en http://localhost:${port}`)
+	
+	})
 })
+
